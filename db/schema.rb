b969a7fc9db856700eccf770cff24cf46b2b1641
biznezo <viettrantrong@gmail.com>
2015-04-10 12:41:03 +0700

    edit book view
    dsad

--- db/schema.rb
+++ db/schema.rb
@@ -55,9 +55,9 @@ ActiveRecord::Schema.define(version: 20150320022357) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "book_id",     limit: 4
-    t.string   "description", limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "likes", force: :cascade do |t|
