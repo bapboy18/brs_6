23dd9f40aac40f31205024c120f247c90ebae7e3
biznezo <viettrantrong@gmail.com>
2015-04-10 09:13:24 +0700

    admin add and edit category
    dsad
    
    dsad
    
    dsad
    
    dsad

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,39 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = "Create a new category successfully!"
+      redirect_to admin_categories_path
+    else
+      flash[:danger] = "Invalid information of category!"
+      render "new"
+    end
+  end
+
+  def edit
+    @category = Category.find params[:id]
+  end
+
+  def update
+    @category = Category.find params[:id]
+    if @category.update_attributes category_params
+      flash[:success]= "Category updated"
+      redirect_to admin_categories_url
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit :name
+  end
+end
