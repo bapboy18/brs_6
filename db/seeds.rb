ac68a7356196b71d8a4922f0efc0044ee214405b
bapboy18 <bapboy18@yahoo.com.vn>
2015-05-11 02:21:46 +0700

    new home 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |n|
+	category_id = rand(10) + 1
+	title = Faker::Name.name
+	author = Faker::Name.name
+	number_pages = 100 + rand(1000)
+	rate_mark_total = 500 + rand(2000)
+	rate_number = rate_mark_total/4
+	Book.create!(category_id: category_id, title: title, author: author,
+							 number_pages: number_pages, rate_mark_total:rate_mark_total,
+							 rate_number: rate_number)
+end
